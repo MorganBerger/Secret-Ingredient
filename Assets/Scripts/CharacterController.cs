bd8e522ef73dd7bdb00a41cfa42f88c606468aa7
Morgan Berger <morgan.berger71@gmail.com>
2026-01-27 00:52:42 +0100

    feat: jump !

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CharacterController : MonoBehaviour
 {
+    [SerializeField] private float _jumpForce = 400f;
+    [SerializeField] private bool _jump = false;
+    private bool _canDoubleJump = false;
+    
     public float moveSpeed = 2f;
     private float horizontalInput;
 
-    // private 
+    [SerializeField] private bool _grounded;
 
     private Rigidbody2D rb;
     private Animator anim;
+
+    public LayerMask whatIsGround;
+
+    public Transform _groundCheck;
+    public Transform _wallCheck;
+
+    public UnityEvent OnLandEvent;
+
+    const float _groundedRadius = .2f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
+
+    void Jump()
+    {
+        if (_grounded && _jump)
+        {
+            // Add a vertical force to the player.
+            anim.SetBool("IsJumping", true);
+            anim.SetBool("JumpUp", true);
+            _grounded = false;
+            rb.AddForce(new Vector2(0f, _jumpForce));
+            _canDoubleJump = true;
+            // particleJumpDown.Play();
+            // particleJumpUp.Play();
+        }
+        _jump = false;
     }
 
     // Update is called once per frame
@@ -36,7 +66,14 @@ public class CharacterController : MonoBehaviour
             moving = true;
         }
 
-        // horizontalInput = Input.GetAxis("Horizontal") > 0 ? 1 : (Input.GetAxis("Horizontal") < 0 ? -1 : 0);
+        if (Input.GetKeyDown(KeyCode.Z))
+		{
+			_jump = true;
+		}
+        if (Input.GetKeyUp(KeyCode.Z) && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+        }
 
         anim.SetBool("Moving", moving);
 
@@ -50,5 +87,32 @@ public class CharacterController : MonoBehaviour
     {
         Debug.Log("H Input: " + horizontalInput);
         rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+
+        bool wasGrounded = _grounded;
+		_grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position, _groundedRadius, whatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+				_grounded = true;
+				if (!wasGrounded)
+				{
+					OnLandEvent.Invoke();
+					// if (!m_IsWall && !isDashing) 
+                    // {
+						// particleJumpDown.Play();
+                    // }
+					_canDoubleJump = true;
+					if (rb.linearVelocity.y < 0f)
+                    {
+						// limitVelOnWallJump = false;
+                    }
+				}
+		}
+
+        Jump();
     }
 }
